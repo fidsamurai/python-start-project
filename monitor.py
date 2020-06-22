c492ecb3941aa17b3b3446b95d760bec69a40d9e
fidsamurai <firdausbeckham23@gmail.com>
2020-06-22 16:19:45 +0530

    Successful functions

--- monitor.py
+++ monitor.py
@@ -10,22 +10,29 @@ cpu_thresh = 50.0
 cpu_pct = psutil.cpu_percent(interval=1)
 
 if cpu_pct >= cpu_thresh:
-    print("CPU Warning, CPU at ",cpu_pct, "percent")
+    cpu_alert = "CPU Warning, CPU at ",cpu_pct, "percent"
+else:
+    cpu_alert = ""
 
 mem = psutil.virtual_memory()
 mem_thresh = 1024 * 1024 * 1024 #change the end value to choose the amount of MB
 
 if mem_thresh >= mem.available:
-    print("Memory Usage Warning only", round((mem.available /1024 /1024), 2), "MB available")
-
+    mem_alert = "Memory Usage Warning only", round((mem.available /1024 /1024), 2), "MB available"
+else:
+    mem_alert = ""
 partition1 = '/'
 disk1 = psutil.disk_usage(partition1)
 disk_thresh = 85.0
 
 if disk_thresh <= disk1[3]:
     disk_alert = print("Root volume usage warning", disk1[3], "% used")
+else:
+    disk_alert = ""
 
 def net_usage(inf = "eth0"):   #change the inf variable according to the interface
+  global net_in_alert
+  global net_out_alert
   net_in_ps1 = psutil.net_io_counters(pernic=True, nowrap=True)[inf]
   net_in_1 = net_in_ps1.bytes_recv
   net_out_1 = net_in_ps1.bytes_sent
@@ -38,19 +45,44 @@ def net_usage(inf = "eth0"):   #change the inf variable according to the interfa
   net_in_thresh = 1.5
   net_out_thresh = 1.5
   if net_in_res <= net_in_thresh:
-     net_in_alert = print(f"Current net-usage:\nIN: {net_in_res} MB/s")
+      net_in_alert = f"Current net-usage:\nIN:", net_in_res, "MB/s"
+  else:
+      net_in_alert = ""
   if net_out_res <= net_out_thresh:
-     net_out_alert = print(f"Current net-usage:\nOUT: {net_out_res} MB/s")
-
+     net_out_alert = f"Current net-usage:\nOUT:", net_out_res, "MB/s"
+  else:
+      net_out_alert = "" 
 net_usage()
 
-def alerts(disk_alert,net_in_alert,net_out_alert):
-    smtp_deets = smtplib.SMTP_TLS( 'smtp.gmail.com', 587)
+message_list = []
+
+if cpu_alert == "":
+    pass
+else:
+    message_list.append(cpu_alert)
+if mem_alert == "":
+    pass
+else:
+    message_list.append(mem_alert)
+if disk_alert == "":
+    pass
+else:
+    message_list.append(disk_alert)
+if net_in_alert == "":
+    pass
+else:
+    message_list.append(net_in_alert)
+if net_out_alert == "":
+    pass
+else:
+    message_list.append(net_out_alert)
+
+print(message_list)
+def alerts():
     sender = 'faga@linuxlab.org'
     receivers = ['faga@linuxlab.org']
-    message = ['disk_alert','net_in_alert','net_out_alert']
-    try:
-        server.login(sender, "redhat237")
-        server.sendmail(sender, receivers, message)
-    except Exception as e:
-        raise
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.login(sender, "redhat237")
+    server.sendmail(sender, receivers, str(message_list))
+
+alerts()
